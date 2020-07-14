b93127e09a45be461ebaaf7ff5c84f41605a868d
stergina <45051285+Stergina@users.noreply.github.com>
2020-07-15 02:45:57 +0300

    Update auebsh-3.c

--- Self-Made Shell Program/auebsh-3.c
+++ Self-Made Shell Program/auebsh-3.c
@@ -4,45 +4,41 @@
 #include <string.h>
 #include "auebsh.h"
 
-int main()
-{
-	int id = 3;	//Αναφέρεται στον τρίτο φλοιό.
-	int done = 0;
+int main() {
+    int id = 3; //Αναφέρεται στον τρίτο φλοιό.
+    int done = 0;
 
-	while(done==0)
-	{
-		printf("auebsh3>");	 //Εμφανίζει το όνομα του φλοιού ως προτροπή.
-		char string[255];
-		fgets(string,255,stdin);	 //Ανάγνωση γραμμής εντολών από το τερματικό.
+    while (done == 0) {
+        printf("auebsh3>"); //Εμφανίζει το όνομα του φλοιού ως προτροπή.
+        char string[255];
+        fgets(string, 255, stdin); //Ανάγνωση γραμμής εντολών από το τερματικό.
 
-		if (sizeof(string)>255){	//Δεν θα τρέξει για string μεγέθους > 255.
-		}
-		else if (feof(stdin)){	  //Έλεγχος για end of file.
-			done = 1;
-		}else{
-			pid_t pid, waitPid;	//Ορισμός μεταβλητών της fork.
-			int  status;
-			pid = fork();	//Σπάει το πρόγραμμα σε δυο προγράμματα.
+        if (sizeof(string) > 255) { //Δεν θα τρέξει για string μεγέθους > 255.
+        } else if (feof(stdin)) { //Έλεγχος για end of file.
+            done = 1;
+        } else {
+            pid_t pid, waitPid; //Ορισμός μεταβλητών της fork.
+            int status;
+            pid = fork(); //Σπάει το πρόγραμμα σε δυο προγράμματα.
 
-			if (pid < 0) {	//Έλεγχος για την fork.
-				return -1;	 //Η fork απέτυχε.
-			}
+            if (pid < 0) { //Έλεγχος για την fork.
+                return -1; //Η fork απέτυχε.
+            }
 
-			if (pid == 0) {	//Διεργασία παιδιού.
-				done = handler(id, string);	//Εκτελεί την handler για id = 3. 
-				return 0;	//Το παιδί σταματάει να εκτελείται.
-			}
-			else {	//Διεργασία πατέρας.
-				waitPid = wait(&status);	  //Περιμένει τον τερματισμό του παιδιού.
-				if (waitPid == -1) {
-					return -1;	//Το Waitpid απέτυχε.
-				}
-			}
+            if (pid == 0) { //Διεργασία παιδιού.
+                done = handler(id, string); //Εκτελεί την handler για id = 3. 
+                return 0; //Το παιδί σταματάει να εκτελείται.
+            } else { //Διεργασία πατέρας.
+                waitPid = wait( & status); //Περιμένει τον τερματισμό του παιδιού.
+                if (waitPid == -1) {
+                    return -1; //Το Waitpid απέτυχε.
+                }
+            }
 
-		}
+        }
 
- 	}//while
-	printf("\n");
-	return 0;
+    } //while
+    printf("\n");
+    return 0;
 
-}//main
+} //main
